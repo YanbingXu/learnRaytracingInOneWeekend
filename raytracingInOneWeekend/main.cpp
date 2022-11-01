fb428ecdd27b1cafe2357a474bc7a93a83692cf4
YanbingXu <605224031@qq.com>
2022-11-01 18:45:42 +0800

    Initial Commit

---
+++ raytracingInOneWeekend/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  raytracingInOneWeekend
+//
+//  Created by 徐雁冰edith on 2022/11/1.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
